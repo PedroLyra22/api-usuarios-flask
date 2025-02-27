4479dd3a6a25011b9c96a0ea5226f5ead3c08d66
Pedro <pedrocalezanelyra@gmail.com>
2025-02-27 18:43:26 -0300

    feat: refactoring main application

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-from flask import Flask
-import pymysql
+from flask import Flask, request, jsonify
+from app.repositories.repositories import UserRepository
+from app.models.models import User
+import hashlib
 
 app = Flask(__name__)
-
-pymysql.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="user_system",
-        port=3306,
-        cursorclass=pymysql.cursors.DictCursor
-    )
 
 @app.route('/')
 def home():
     return 'deu bom'
+
+@app.route("/register", methods=["POST"])
+def register():
+    data = request.json
+    hashed_password = hashlib.sha256(data["password"].encode()).hexdigest()
+    user = User(name=data["name"], email=data["email"], password=hashed_password)
+
+    try:
+        UserRepository.create(user)
+        return jsonify({"message": "User created successfully"}), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
