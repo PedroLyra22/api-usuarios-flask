2201108e50546e2e8cd74db4ae5b27906a61d9cc
Pedro <pedrocalezanelyra@gmail.com>
2025-02-27 18:42:52 -0300

    fix: add missing id

--- app/models/models.py
+++ app/models/models.py
@@ -1,5 +1,6 @@
 class User:
     def __init__(self, id = None, name = None, email = None, password = None):
+        self.id = id
         self.name = name
         self.email = email
         self.password = password
